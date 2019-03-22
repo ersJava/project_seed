48bca249adfe7334c84b0fc35f4faec5d8de7fc0
AMallik <Ashraful.mallik100@gmail.com>
2019-03-21 20:24:55 -0400

    Added Login Page for the 12th Time

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
 import Post from "./Post";
+import Cards from "./Card";
+import Logo from "./Logo";
+import LoginBtn from "./LoginBtn";
 
 
 const App = () => (
   <div className="App">
-    <h1>Project Seed</h1>
+   
     <Post />
+    {/* =================Panel Layout =================*/}
+    <div className="ui  center aligned padded grid">
+            <div className="ten wide white column leftPanel">
+              <Cards />
+            
+            </div>
+
+            <div className="six wide column rightPanel">
+              <Logo/>
+              <LoginBtn />
+              <br></br>
+            </div>
+          </div>
+          {/* =================Panel Layout================= */}
   </div>
 )
 
