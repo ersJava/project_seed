a48aa8b0edce53423e8a36019d341e4af714cdbe
Shohan <ashraful.mallik100@gmail.com>
2019-03-29 23:44:16 -0400

    Fixed menu

--- client/src/Components/Sidemenu.js
+++ client/src/Components/Sidemenu.js
@@ -8,6 +8,19 @@ const Side= () => (
   
 
   <div className="sideMenu">
+   <div className="customItem">
+   <i class="users icon"></i> &nbsp;
+    Start A Team 
+   </div>
+   <div className="customItem">
+   <i class="child icon"></i> &nbsp;
+   Join A Team
+   </div>
+   <div className="customItem">
+   <i class="lightbulb outline icon"></i> &nbsp;
+   
+   Post A Project Idea
+   </div>
   </div>
   
     
